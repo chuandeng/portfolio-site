1cf43e58edc3d7d3ef40c414f553ae9f8bf36cf9
Colin Huang <chuandeng99@sohu.com>
2026-04-09 22:19:45 +0800

    fix auto build issues

--- docs/requirement.md
+++
@@ -1,5 +0,0 @@
-项目名称: lulu的站点
-个人网站
-
-技术栈: Vue3 + Vite + Rspack + pnpm + vue-i18n + vue router
-
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/self-site-vue/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/portfolio-site/' : '/',
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
